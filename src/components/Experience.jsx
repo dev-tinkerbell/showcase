28fa2ca41bb266a2939f7ab692a74598f247cc00
Claire.kim <claire.kim@cheilpengtai.com>
2023-09-13 18:47:37 +0900

    npm 추가, 소스수정

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,207 +1,164 @@
-import {
-  Float,
-  Html,
-  OrbitControls,
-  PerspectiveCamera,
-  useScroll,
-} from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
-import { useMemo, useRef } from "react";
-import * as THREE from "three";
-import { Airplane } from "./Airplane";
-import { Background } from "./Background";
-import { Plant01 } from "./Plant01";
-import { Plant02 } from "./Plant02";
-import { Plant03 } from "./Plant03";
-import { Cloud } from "./Cloud";
-import { Cloud2 } from "./Cloud2";
-import { Environment, Stars } from "@react-three/drei";
+import { Float, PerspectiveCamera, useScroll } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import { useMemo, useRef } from 'react';
+import * as THREE from 'three';
+import { Airplane } from './Airplane';
+import { Background } from './Background';
+import { Plant01 } from './Plant01';
+import { Plant02 } from './Plant02';
+import { Plant03 } from './Plant03';
+import { Cloud } from './Cloud';
+import { Cloud2 } from './Cloud2';
+import { Environment, Stars } from '@react-three/drei';
 const LINE_NB_POINTS = 12000;
 
 export const Experience = () => {
-  const curve = useMemo(() => {
-    return new THREE.CatmullRomCurve3(
-      [
-        new THREE.Vector3(0, 0, 0),
-        new THREE.Vector3(0, 0, -10),
-        new THREE.Vector3(-2, 0, -20),
-        new THREE.Vector3(-3, 0, -30),
-        new THREE.Vector3(0, 0, -40),
-        new THREE.Vector3(5, 0, -50),
-        new THREE.Vector3(7, 0, -60),
-        new THREE.Vector3(5, 0, -70),
-        new THREE.Vector3(0, 0, -80),
-        new THREE.Vector3(0, 0, -90),
-        new THREE.Vector3(0, 0, -100),
-        new THREE.Vector3(-1, 0, -103),
-        new THREE.Vector3(0, 0, -105),
-        new THREE.Vector3(1, 0, -103),
-        new THREE.Vector3(0, 0, -101),
-      ],
-      false,
-      "catmullrom",
-      0.5
-    );
-  }, []);
+    const curve = useMemo(() => {
+        return new THREE.CatmullRomCurve3(
+            [
+                new THREE.Vector3(0, 0, 0),
+                new THREE.Vector3(0, 0, -10),
+                new THREE.Vector3(-2, 0, -20),
+                new THREE.Vector3(-3, 0, -30),
+                new THREE.Vector3(0, 0, -40),
+                new THREE.Vector3(5, 0, -50),
+                new THREE.Vector3(7, 0, -60),
+                new THREE.Vector3(5, 0, -70),
+                new THREE.Vector3(0, 0, -80),
+                new THREE.Vector3(0, 0, -90),
+                new THREE.Vector3(0, 0, -100),
+                new THREE.Vector3(-1, 0, -103),
+                new THREE.Vector3(0, 0, -105),
+                new THREE.Vector3(1, 0, -103),
+                new THREE.Vector3(0, 0, -101),
+            ],
+            false,
+            'catmullrom',
+            0.5
+        );
+    }, []);
 
-  const linePoints = useMemo(() => {
-    return curve.getPoints(LINE_NB_POINTS);
-  }, [curve]);
+    const linePoints = useMemo(() => {
+        return curve.getPoints(LINE_NB_POINTS);
+    }, [curve]);
 
-  const shape = useMemo(() => {
-    const shape = new THREE.Shape();
-    shape.moveTo(0, -0.2);
-    // shape.lineTo(0, 0.2);
-    return shape;
-  }, [curve]);
+    const shape = useMemo(() => {
+        const shape = new THREE.Shape();
+        // shape.moveTo(0, -0.2);
+        // shape.lineTo(0, 0.2);
+        return shape;
+    }, [curve]);
 
-  const cameraGroup = useRef();
-  const scroll = useScroll();
+    const cameraGroup = useRef();
+    const scroll = useScroll();
 
-  useFrame((_state, delta) => {
-    const curPointIndex = Math.min(
-      Math.round(scroll.offset * linePoints.length),
-      linePoints.length - 1
-    );
-    const curPoint = linePoints[curPointIndex];
-    const pointAhead =
-      linePoints[Math.min(curPointIndex + 1, linePoints.length - 1)];
+    useFrame((_state, delta) => {
+        // console.log('scroll', scroll);
 
-    const xDisplacement = (pointAhead.x - curPoint.x) * 80;
+        const curPointIndex = Math.min(Math.round(scroll.offset * linePoints.length), linePoints.length - 1);
+        const curPoint = linePoints[curPointIndex];
+        const pointAhead = linePoints[Math.min(curPointIndex + 1, linePoints.length - 1)];
 
-    // Math.PI / 2 -> LEFT
-    // -Math.PI / 2 -> RIGHT
+        const xDisplacement = (pointAhead.x - curPoint.x) * 80;
 
-    const angleRotation =
-      (xDisplacement < 0 ? 1 : -1) *
-      Math.min(Math.abs(xDisplacement), Math.PI / 3);
+        // Math.PI / 2 -> LEFT
+        // -Math.PI / 2 -> RIGHT
 
-    const targetAirplaneQuaternion = new THREE.Quaternion().setFromEuler(
-      new THREE.Euler(
-        airplane.current.rotation.x,
-        airplane.current.rotation.y,
-        angleRotation
-      )
-    );
-    const targetCameraQuaternion = new THREE.Quaternion().setFromEuler(
-      new THREE.Euler(
-        cameraGroup.current.rotation.x,
-        angleRotation,
-        cameraGroup.current.rotation.z
-      )
-    );
+        const angleRotation = (xDisplacement < 0 ? 1 : -1) * Math.min(Math.abs(xDisplacement), Math.PI / 3);
 
-    airplane.current.quaternion.slerp(targetAirplaneQuaternion, delta * 2);
-    cameraGroup.current.quaternion.slerp(targetCameraQuaternion, delta * 2);
+        const targetAirplaneQuaternion = new THREE.Quaternion().setFromEuler(
+            new THREE.Euler(airplane.current.rotation.x, airplane.current.rotation.y, angleRotation)
+        );
+        const targetCameraQuaternion = new THREE.Quaternion().setFromEuler(
+            new THREE.Euler(cameraGroup.current.rotation.x, angleRotation, cameraGroup.current.rotation.z)
+        );
 
-    cameraGroup.current.position.lerp(curPoint, delta * 24);
-  });
+        airplane.current.quaternion.slerp(targetAirplaneQuaternion, delta * 2);
+        cameraGroup.current.quaternion.slerp(targetCameraQuaternion, delta * 2);
 
-  const airplane = useRef();
+        cameraGroup.current.position.lerp(curPoint, delta * 24);
+    });
 
-  return (
-    <>
-      {/*<OrbitControls enableZoom={false} />*/}
-      <Stars
-        radius={10}
-        depth={70}
-        count={800}
-        factor={4}
-        saturation={10}
-        fade
-        speed={4}
-      />
+    const airplane = useRef();
 
-      <pointLight position={[0, 0, 0]} intensity={1} />
-      <pointLight position={[-10, 10, -3]} intensity={50} color={"black"} />
-      <pointLight position={[2.5, -0.5, -2]} intensity={50} color={"black"} />
-      <pointLight position={[0, -2.5, -0.5]} intensity={50} color={"purple"} />
-      <pointLight position={[0, -4.5, -0.5]} intensity={500} color={"purple"} />
-      <pointLight position={[3, 4, -5]} intensity={100} color={"black"} />
-      <pointLight position={[0, 2, -50]} intensity={200} color={"black"} />
-      <pointLight position={[-7, -1, -100]} intensity={900} color={"purple"} />
-      <pointLight position={[7, 7, -100]} intensity={900} color={"purple"} />
+    return (
+        <>
+            {/*<OrbitControls enableZoom={false} />*/}
+            <Stars radius={10} depth={70} count={800} factor={4} saturation={10} fade speed={4} />
 
-      <group ref={cameraGroup}>
-        <Background />
-        <PerspectiveCamera position={[0.2, 0.5, 6.9]} fov={30} makeDefault />
-        <group ref={airplane}>
-          <Float floatIntensity={2} speed={2}>
-            <Airplane
-              rotation-y={Math.PI / 2}
-              scale={[0.5, 0.5, 0.5]}
-              position-y={0.2}
-              position-z={1}
+            <pointLight position={[0, 0, 0]} intensity={1} />
+            <pointLight position={[-10, 10, -3]} intensity={50} color={'black'} />
+            <pointLight position={[2.5, -0.5, -2]} intensity={50} color={'black'} />
+            <pointLight position={[0, -2.5, -0.5]} intensity={50} color={'purple'} />
+            <pointLight position={[0, -4.5, -0.5]} intensity={500} color={'purple'} />
+            <pointLight position={[3, 4, -5]} intensity={100} color={'black'} />
+            <pointLight position={[0, 2, -50]} intensity={200} color={'black'} />
+            <pointLight position={[-7, -1, -100]} intensity={900} color={'purple'} />
+            <pointLight position={[7, 7, -100]} intensity={900} color={'purple'} />
+
+            <group ref={cameraGroup}>
+                <Background />
+                <PerspectiveCamera position={[0.2, 0.5, 6.9]} fov={30} makeDefault />
+                <group ref={airplane}>
+                    <Float floatIntensity={2} speed={2}>
+                        <Airplane rotation-y={Math.PI / 2} scale={[0.5, 0.5, 0.5]} position-y={0.2} position-z={1} />
+                    </Float>
+                </group>
+            </group>
+
+            {/* LINE */}
+            <group position-y={-2}>
+                <mesh>
+                    <extrudeGeometry
+                        args={[
+                            shape,
+                            {
+                                steps: LINE_NB_POINTS,
+                                bevelEnabled: false,
+                                extrudePath: curve,
+                            },
+                        ]}
+                    />
+                    <meshStandardMaterial color={'white'} opacity={0.7} transparent />
+                </mesh>
+            </group>
+
+            {/* <Float floatIntensity={2} speed={2} enabled={false}> */}
+            <Float floatIntensity={2} speed={2} enabled={scroll.offset > 0.1 ? false : true}>
+                <Plant01
+                    opacity={0.95}
+                    scale={[0.12, 0.12, 0.12]}
+                    position={[-1.5, -1, -3]}
+                    rotation={[Math.PI / 3, Math.PI / 3, 0]}
+                />
+            </Float>
+
+            <Plant01 opacity={0.93} scale={[0.08, 0.08, 0.08]} position={[-6.5, -1, -8]} rotation={[0, 0, 0]} />
+
+            <Plant01 opacity={0.95} scale={[0.1, 0.1, 0.1]} position={[2.5, -0.5, -2]} rotation={[0, 0, 0]} />
+
+            <Plant02
+                opacity={0}
+                scale={[0.6, 0.6, 0.6]}
+                position={[3, 1, -12]}
+                rotation={[Math.PI / 2, Math.PI / 2, 0]}
             />
-          </Float>
-        </group>
-      </group>
 
-      {/* LINE */}
-      <group position-y={-2}>
-        <mesh>
-          <extrudeGeometry
-            args={[
-              shape,
-              {
-                steps: LINE_NB_POINTS,
-                bevelEnabled: false,
-                extrudePath: curve,
-              },
-            ]}
-          />
-          <meshStandardMaterial color={"white"} opacity={0.7} transparent />
-        </mesh>
-      </group>
-
-      <Plant01
-        opacity={0.95}
-        scale={[0.12, 0.12, 0.12]}
-        position={[-1.5, -1, -3]}
-        rotation={[Math.PI / 3, Math.PI / 3, 0]}
-      />
-
-      <Plant01
-        opacity={0.93}
-        scale={[0.08, 0.08, 0.08]}
-        position={[-6.5, -1, -8]}
-        rotation={[0, 0, 0]}
-      />
-
-      <Plant01
-        opacity={0.95}
-        scale={[0.1, 0.1, 0.1]}
-        position={[2.5, -0.5, -2]}
-        rotation={[0, 0, 0]}
-      />
-
-      <Plant02
-        opacity={0}
-        scale={[0.6, 0.6, 0.6]}
-        position={[3, 1, -12]}
-        rotation={[Math.PI / 2, Math.PI / 2, 0]}
-      />
-
-      {/* <Cloud2
+            {/* <Cloud2
         opacity={0.99}
         scale={[0.8, 0.8, 0.8]}
         position={[-1, -1, -53]}
         rotation={[0, 0, 0]}
       /> */}
 
-      <Plant01
-        opacity={0.97}
-        scale={[0.2, 0.2, 0.2]}
-        position={[-1, -1, -53]}
-        rotation={[0, 0, 0]}
-      />
+            <Plant01 opacity={0.97} scale={[0.2, 0.2, 0.2]} position={[-1, -1, -53]} rotation={[0, 0, 0]} />
 
-      <Plant01
-        opacity={0.97}
-        scale={[0.2, 0.2, 0.2]}
-        position={[0, 1, -100]}
-        rotation={[Math.PI * 2.5, Math.PI / 2, 0]}
-      />
-    </>
-  );
+            <Plant01
+                opacity={0.97}
+                scale={[0.2, 0.2, 0.2]}
+                position={[0, 1, -100]}
+                rotation={[Math.PI * 2.5, Math.PI / 2, 0]}
+            />
+        </>
+    );
 };
